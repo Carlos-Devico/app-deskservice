6e6aacfdfe7fce1abebb3c9806498b2138af0232
carlos-devico <carlosdevico@gmail.com>
2023-08-07 17:05:59 -0300

    creado o projeto backend

---
+++ src/main/java/com/vdm/deskservice/DeskserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vdm.deskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vdm/deskservice/DeskserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdm.deskservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
