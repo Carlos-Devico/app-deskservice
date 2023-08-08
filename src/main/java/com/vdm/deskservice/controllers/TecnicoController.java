3c207220d1351dc2923933768cd6b48e790cfc29
carlos-devico <carlosdevico@gmail.com>
2023-08-08 04:22:15 -0300

    protcriado endpoint findById para tecnico

--- src/main/java/com/vdm/deskservice/controllers/TecnicoController.java
+++ src/main/java/com/vdm/deskservice/controllers/TecnicoController.java
@@ -1,4 +1,25 @@
-package com.vdm.deskservice.resources;
+package com.vdm.deskservice.controllers;
 
-public class TecnicoResource {
+import com.vdm.deskservice.domain.Tecnico;
+import com.vdm.deskservice.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoController {
+    @Autowired
+    private TecnicoService service;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+        Tecnico obj = service.localizaPeloId(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
 }
--- src/main/java/com/vdm/deskservice/services/TecnicoService.java
+++ src/main/java/com/vdm/deskservice/services/TecnicoService.java
@@ -1,2 +1,19 @@
-package com.vdm.deskservice.services;public class TecnicoService {
+package com.vdm.deskservice.services;
+
+import com.vdm.deskservice.domain.Tecnico;
+import com.vdm.deskservice.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico localizaPeloId(Integer id){
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
 }
