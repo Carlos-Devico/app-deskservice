69bd0a0fb858e80766d75e80f9dee89865f17543
carlos-devico <carlosdevico@gmail.com>
2023-08-08 00:11:25 -0300

    criada configurações de teste

--- src/main/java/com/vdm/deskservice/config/TestConfig.java
+++ src/main/java/com/vdm/deskservice/config/TestConfig.java
@@ -1,2 +1,18 @@
-package com.vdm.deskservice;public class TestConfig {
+package com.vdm.deskservice.config;
+
+import com.vdm.deskservice.services.DBServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+    @Autowired
+    private DBServices dbServices;
+    @Bean
+    public void instanciaDB(){
+        this.dbServices.instaciaDB();
+    }
 }
