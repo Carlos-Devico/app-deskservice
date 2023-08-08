101df66e0fba47dc32f6974e54ae890bbe6bc871
carlos-devico <carlosdevico@gmail.com>
2023-08-07 21:32:54 -0300

    implementado annotations nas entidades.

--- src/main/java/com/vdm/deskservice/domain/Cliente.java
+++ src/main/java/com/vdm/deskservice/domain/Cliente.java
@@ -1,9 +1,15 @@
 package com.vdm.deskservice.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
+@Entity
+public class Cliente extends Pessoa {
+
+    private static final long serialVersionUID = 1L;
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente() {
